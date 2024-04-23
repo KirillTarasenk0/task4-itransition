b50df41ad713e7667c45c91b3d23f9d1091e8d74
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-04-23 04:29:00 +0300

    Showing user's data in table was made

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,9 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\SecurityBundle\Security;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractController
 {
+    public function __construct(private readonly UserRepository $userRepository,)
+    {
+    }
+
     #[Route('/usersPage', name: 'app_users_page')]
     public function index(Security $security, EntityManagerInterface $entityManager): Response
     {
@@ -19,7 +24,26 @@ class UserController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user_name' => $user->getName(),
+            'users' => $this->userRepository->findAll(),
         ]);
+    }
+    #[Route('/deleteUsers', name: 'delete_users')]
+    public function blockUsers(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->json(['ok']);
+    }
+    #[Route('/unblockUsers', name: 'unblock_users')]
+    public function unblockUsers(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->json(['ok']);
+    }
+    #[Route('/deleteUsers', name: 'delete_users')]
+    public function deleteUsers(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->json(['ok']);
     }
 }
